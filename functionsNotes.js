5f6db7aeab776c6e6559fe0a26aa95db57aff8b9
6exf <6exf@fresnou.org>
2023-03-14 13:55:57 -0700

    initial commit

---
+++ functionsNotes.js
@@ -0,0 +1,42 @@
+//Functions Notes
+//Step 1 - Define the Function
+function sayHello() {
+    console.log("Hello");
+}
+//Step 2 - Call (use) the Function
+//sayHello();
+//sayHello();
+//sayHello();
+//sayHello();
+
+// function printMax(){
+//     let x =6;
+//     let y =29;
+//     if (x>y){
+//         console.log(`the larger number ${x}`);
+//     }else {
+//         console.log(`the larger number ${y}`);
+//     }
+// }
+//
+// printMax();
+
+// function printMax(x, y) {
+//     if (x>y){
+//         console.log(`the larger number ${x}`);
+//     } else {
+//         console.log(`the larger number ${y}`);
+//     }
+// }
+//
+// printMax(14,15);
+// printMax(7,7);
+// printMax(21,20);
+// printMax(0,-1);
+
+function sum(number1, number2){
+    console.log(`${number1} + ${number2} = ${number1+number2}`);
+}
+
+sum(number1:7, number2:14);
+sum(number1:2, number2:22);
