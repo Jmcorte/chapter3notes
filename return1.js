2e3d65cddd4687f011ddbdc343986cac35765394
6exf <6exf@fresnou.org>
2023-03-17 14:18:30 -0700

    initial commit

--- return1.js
+++ return1.js
@@ -9,3 +9,17 @@ function nameSwapper() {
 let myName = nameSwapper();
 console.log(myName);
 
+function numberGenerator() {
+    let first = readLine.question("First Number : ");
+    let last = readLine.question("Second Number : ");
+    return(`${first+last}`);
+
+}
+
+function avg(t1,t2,t3, t4,t5,t6,t7){     
+    let answer = t1+t2+t3+t4+t5+t6+t7;
+    answer = answer / 7;
+    answer = answer.toFixed(2);
+    return(answer) }
+    let total = avg(7,8,9,0,4,3,2);
+console.log(total);
